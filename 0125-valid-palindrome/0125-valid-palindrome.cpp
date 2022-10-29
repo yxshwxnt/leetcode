935cf4f79d72c8e41e8b61a8af4d7670655e526d
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-10-30 03:05:21 +0530

    Time: 20 ms (19.00%), Space: 7.3 MB (78.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0,right=s.length()-1; 
+        while(left<right){
+            if(!isalnum(s[left])){       //isalnum ignores all the spaces,nos and commas
+                left++;             //increase kiya index,  -> ignore hogaya
+            }
+            else if(!isalnum(s[right])){
+                right--; 
+            }
+            else if(tolower(s[left])!=tolower(s[right])){
+                return false; 
+            }
+            else{
+                left++; 
+                right--; 
+            }
+        }
+        return true; 
+    }
+};

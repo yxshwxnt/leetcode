a154913f4a58b234e3e6ab8048dca71714ec0efc
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-10-30 01:48:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.67%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]); 
+            }
+            else{
+                if(st.size()==0)
+                    return false; 
+                char ch=st.top(); 
+                st.pop(); 
+                if(ch=='(' && s[i]==')' || ch=='{' && s[i]=='}' || ch=='[' && s[i]==']'){
+                    continue; 
+                }
+                else{
+                    return false; 
+                }
+            }
+        }
+        return st.empty(); 
+    }
+};

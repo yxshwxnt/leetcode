3649de1c3bf731ea229d0c360b366b5d22d9be04
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-12-01 02:10:06 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,37 +4,43 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  public:
-    bool checkCycle(int node,vector<int> adj[],int vis[],int dfsVis[]){
-        vis[node]=1; 
-        dfsVis[node]=1; 
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(checkCycle(it,adj,vis,dfsVis)){
-                    return true; 
-                }
-            }
-            else if(dfsVis[it]){
-                return true; 
-            }
-        }
-        dfsVis[node]=0; 
-        return false;
-    }
-    // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        int vis[V],dfsVis[V]; 
-        memset(vis,0,sizeof vis);
-        memset(dfsVis,0,sizeof dfsVis);
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(checkCycle(i,adj,vis,dfsVis)){
-                    return true; 
-                }
-            }
-        }
-        return false; 
-    }
+	public:
+	//Using topo Sort
+	//Function to return list containing vertices in Topological order. 
+	bool isCyclic(int V, vector<int> adj[]) 
+	{
+	    int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int> q; 
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i); 
+	        }
+	    }
+	    int cnt=0; 
+	    //o(v+e)
+	    while(!q.empty()){
+	        int node=q.front(); 
+	        q.pop(); 
+	        cnt++; 
+	        //node is in your topo sort 
+	        //so we remove it from indegree 
+	        for(auto it:adj[node]){
+	            indegree[it]--; 
+	            if(indegree[it]==0){
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    if(cnt==V){
+	        return false; 
+	    }
+	    return true; 
+	}
 };
 
 //{ Driver Code Starts.

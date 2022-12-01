2abe83d5e6a24294721f18a6726e06351900fce3
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-12-01 21:48:29 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int V, vector<pair<int, int> >& prerequisites) {
+	    vector<int> adj[V]; 
+	    for(auto it: prerequisites){
+	        adj[it.first].push_back(it.second);     //crearting adjacenecy list 
+	    }
+	    
+	    //code for topo sort
+	    int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int> q; 
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i); 
+	        }
+	    }
+	    vector<int> topo; 
+	    while(!q.empty()){
+	        int node=q.front(); 
+	        q.pop(); 
+	        topo.push_back(node); 
+	        //node is in your topo sort 
+	        //so we remove it from indegree 
+	        for(auto it:adj[node]){
+	            indegree[it]--; 
+	            if(indegree[it]==0){
+	                q.push(it); 
+	            }
+	        }
+	    } 
+	    
+	    if(topo.size()==V)  return true;   //it is a directed acyclic graph(DAG)
+	    return false; 
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends

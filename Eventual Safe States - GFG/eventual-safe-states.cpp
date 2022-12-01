f72b3b46bfe963ad8eeebe99ca451b5604ec1db6
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-12-01 22:10:25 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -9,49 +9,40 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-        bool dfsCheck(int node, vector<int> adj[], int vis[],
-         int pathVis[], int check[]) {
-        		vis[node] = 1;
-        		pathVis[node] = 1;
-        		check[node] = 0;
-        		// traverse for adjacent nodes
-        		for (auto it : adj[node]) {
-        			// when the node is not visited
-        			if (!vis[it]) {
-        			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
-        					check[node] = 0;
-        					return true;
-        				}
+  public:
+  //using topo sort
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        vector<int> adjRev[V]; 
+        int indegree[V]={0};
+        for(auto i=0;i<V;i++){
+            // i -> it  
+            // it -> i 
+            for(auto it: adj[i]){
+                adjRev[it].push_back(i);     //reversing all connections 
+                indegree[i]++;
+            }
+        }
         
-        			}
-        			// if the node has been previously visited
-        			// but it has to be visited on the same path
-        			else if (pathVis[it]) {
-        				check[node] = 0;
-        				return true;
-        			}
-        		}
-        		check[node] = 1;
-        		pathVis[node] = 0;
-        		return false;
-        	}
-    public:
-    	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
-    		int vis[V] = {0};
-    		int pathVis[V] = {0};
-    		int check[V] = {0};
-    		vector<int> safeNodes;
-    		for (int i = 0; i < V; i++) {
-    			if (!vis[i]) {
-    				dfsCheck(i, adj, vis, pathVis, check);
-    			}
-    		}
-    		for (int i = 0; i < V; i++) {
-    			if (check[i] == 1) safeNodes.push_back(i);
-    		}
-    		return safeNodes;
-    	}
+        queue<int> q; 
+        vector<int> safeNodes; 
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i); 
+            }
+        }
+        
+        while(!q.empty()){
+            int node=q.front(); 
+            q.pop(); 
+            safeNodes.push_back(node); 
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0)   q.push(it); 
+            }
+        }
+        sort(safeNodes.begin(),safeNodes.end()); 
+        return safeNodes; 
+    }
 };
 
 

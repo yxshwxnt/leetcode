cc6d27e266c2573ad0b4a13fc5fc0bece9fc272a
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2022-12-03 20:58:01 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -5,51 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    public:
-    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
-        {
-    
-            // Create a priority queue for storing the nodes as a pair {dist,node}
-            // where dist is the distance from source to the node. 
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    
-            // Initialising distTo list with a large number to
-            // indicate the nodes are unvisited initially.
-            // This list contains distance from source to the nodes.
-            vector<int> distTo(V, INT_MAX);
-    
-            // Source initialised with dist=0.
-            distTo[S] = 0;
-            pq.push({0, S});
-    
-            // Now, pop the minimum distance node first from the min-heap
-            // and traverse for all its adjacent nodes.
-            while (!pq.empty())
-            {
-                int node = pq.top().second;
-                int dis = pq.top().first;
-                pq.pop();
-    
-                // Check for all adjacent nodes of the popped out
-                // element whether the prev dist is larger than current or not.
-                for (auto it : adj[node])
-                {
-                    int v = it[0];
-                    int w = it[1];
-                    if (dis + w < distTo[v])
-                    {
-                        distTo[v] = dis + w;
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        //using a set 
+        set<pair<int,int>> st; 
+        vector<int> dist(V,1e9); 
         
-                        // If current distance is smaller,
-                        // push it into the queue.
-                        pq.push({dis + w, v});
+        st.insert({0,S}); 
+        dist[S]=0; 
+        while(!st.empty()){
+            auto it=*(st.begin()); 
+            int node=it.second; 
+            int dis=it.first; 
+            st.erase(it); 
+            for(auto it:adj[node]){
+                int adjNode=it[0]; 
+                int edgeW=it[1]; 
+                if(dis + edgeW<dist[adjNode]){
+                    //erase if exist 
+                    if(dist[adjNode]!=1e9){
+                        st.erase({dist[adjNode],adjNode}); 
                     }
+                    dist[adjNode]=dis+edgeW; 
+                    st.insert({dist[adjNode],adjNode}); 
                 }
             }
-            // Return the list containing shortest distances
-            // from source to all the nodes.
-            return distTo;
         }
+        return dist; 
+    }
 };
 
 
